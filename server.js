009153147b0fd702af50784d1bb23e455a1acd1c
Foram Sojitra <foram.sojitra295@gmail.com>
2020-03-21 12:13:47 +0530

    useMongoClient removed

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var mongoose = require('mongoose');
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
 
-mongoose.connect('mongodb://localhost/ManualAuth', { useMongoClient: true });
+mongoose.connect('mongodb://localhost/ManualAuth');
 
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
