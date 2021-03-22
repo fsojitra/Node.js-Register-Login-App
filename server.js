4254af20f0536aa49f3f3b8e7eafe7b601ffd230
foramsojitra <foram.sojitra@techifysolutions.com>
2021-03-22 23:35:44 +0530

    change in mongo config

--- server.js
+++ server.js
@@ -7,9 +7,14 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoStore = require('connect-mongo')(session);
 
-const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost/ManualAuth';
+// const MongoDBURI = process.env.MONGO_URI || 'mongodb://localhost/ManualAuth';
+const MongoDBURI =  'mongodb+srv://admin:admin@cluster0.ezsuq.mongodb.net/ManualAuth?retryWrites=true&w=majority';
 
-mongoose.connect(MongoDBURI, { useUnifiedTopology: true });
+// mongoose.connect(MongoDBURI, { useUnifiedTopology: true });
+mongoose.connect(MongoDBURI, {
+  useUnifiedTopology: true,
+  useNewUrlParser: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
